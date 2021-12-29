7b8ede16bad84b4077714f4a2385b8346896cf18
crunchytee <tosh.robertsbrockway@gmail.com>
2021-12-28 20:16:39 -0800

    setup .env

--- app/config.py
+++ app/config.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv()
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "lengthy-password"
-    SQLALCHEMY_DATABASE_URI = "postgresql://tosh:flask-practice@localhost:5432/flask-practice"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    FLASK_APP = os.environ.get("FLASK_APP")
