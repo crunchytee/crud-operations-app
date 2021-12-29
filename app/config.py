8192c7740674cf82c513113542d1f7b714aea7c9
crunchytee <tosh.robertsbrockway@gmail.com>
2021-12-28 19:50:04 -0800

    added flask sqlalchemy

--- app/config.py
+++ app/config.py
@@ -3,4 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "lengthy-password"
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/flask-practice"
+    SQLALCHEMY_DATABASE_URI = "postgresql://tosh:flask-practice@localhost:5432/flask-practice"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
